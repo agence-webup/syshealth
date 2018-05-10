a930f68788d1eba712988aaccbe3ef15c967a908
Matthieu Martin <matthieu@agence-webup.com>
2018-05-10 14:11:03 +0200

    fix memory threshold

--- threshold/memory.go
+++ threshold/memory.go
@@ -10,15 +10,21 @@ func (trigger *MemoryUsageTrigger) GetKey() key {
 }
 
 func (trigger *MemoryUsageTrigger) Check(metrics syshealth.Data) syshealth.ThresholdLevel {
-	if raw, ok := metrics["memory.available"]; ok {
-		if available, ok := raw.(float64); ok {
-			if available <= 0.3 {
-				return syshealth.Critical
-			}
-			if available <= 0.5 {
-				return syshealth.Warning
+
+	if rawUsed, ok := metrics["memory.used_percent"]; ok {
+		if used, ok := rawUsed.(float64); ok && used >= 0.8 {
+			if raw, ok := metrics["memory.available"]; ok {
+				if available, ok := raw.(float64); ok {
+					if available <= 0.3 {
+						return syshealth.Critical
+					}
+					if available <= 0.5 {
+						return syshealth.Warning
+					}
+				}
 			}
 		}
 	}
+
 	return syshealth.None
 }
